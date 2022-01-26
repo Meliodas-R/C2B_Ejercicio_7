753b208ba5371afd00478ee88a2e884467c28c4d
Alex Drask Ivanov <inceptiongodmaxwell@gmail.com>
2022-01-26 12:51:44 +0100

    Lista de cambios - Version 0.2
    -Modificada la clase Banco para aplicar el modificador estatico para sus metodos.
    -Añadidas las pruebas en la clase principal.

--- src/main/java/com/eon/Entities/Banco.java
+++ src/main/java/com/eon/Entities/Banco.java
@@ -1,7 +1,7 @@
 package com.eon.Entities;
 
 public class Banco {
-    
+
     private static Cliente clientes[];
     private static int numeroClientes;
 
@@ -9,21 +9,31 @@ public class Banco {
         clientes = new Cliente[10];
         numeroClientes = 0;
     }
-    
-    private Banco() {
+
+    public Banco() {
     }
-    
+
     public static void agregarCliente(String nombre, String apellido) {
         clientes[numeroClientes] = new Cliente(nombre, apellido);
         numeroClientes++;
     }
-    
+
     public static int getNumeroClientes() {
         return numeroClientes;
     }
 
     public static Cliente getCliente(int posicion) {
         return clientes[posicion];
+    }
+
+    public static void generarInfome() {
+        System.out.println("INFORME CLIENTES");
+        System.out.println("================");
+
+        for (int idCliente = 0; idCliente < Banco.getNumeroClientes(); idCliente++) {
+            Cliente cliente = Banco.getCliente(idCliente);
+            System.out.println(cliente);
+        }
     }
     
 }
--- src/main/java/com/eon/PL/Principal.java
+++ src/main/java/com/eon/PL/Principal.java
@@ -1,9 +1,16 @@
 
 package com.eon.PL;
 
+import com.eon.Entities.Banco;
+
 public class Principal {
     
     public static void main(String[] args) {
+        
+        Banco.agregarCliente("Michar", "Avila");
+        Banco.agregarCliente("Sergio", "Adrian");
+        
+        Banco.generarInfome();
         
     }
 }
